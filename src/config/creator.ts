9961ae91f4c79fab76f5e0e19f304d816935da63
valado <vld.daskalov@gmail.com>
2026-03-23 18:11:41 +0100

    feat: improve project namings

--- src/config/creator.ts
+++ src/config/creator.ts
@@ -5,15 +5,15 @@ export const CREATOR_CONFIG = {
   surveyUrl: 'https://forms.gle/fM81aBrYi393n8mE8',
   tools: [
     {
-      name: 'Tinytoolstack',
+      name: 'tinytoolstack.com',
       url: 'https://tinytoolstack.com/apps/popular',
       description: 'Tiny productivity apps for creators',
       icon: 'https://tinytoolstack.com/icons/logo.svg',
     },
     {
-      name: 'Vibesmonitor',
+      name: 'vibesmonitor.com',
       url: 'https://vibesmonitor.com',
-      description: 'You ship products. Vibesmonitor helps you stay on top of them.',
+      description: 'You ship products. We help you monitor them.',
       icon: 'https://vibesmonitor.com/assets/icons/logo.svg',
     },
   ],
